47207661ed54dbe42f7dd8feaa9be7c49e94b796
Jolvera2001 <jolvera6@stedwards.edu>
2024-03-20 23:41:15 -0500

    started project

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+mod database;
+
+use actix_web::{get, App, HttpResponse, HttpServer, Responder };
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().json("Hello World!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new()
+        .service(hello))
+        .bind(("localhost", 8080))?
+        .run()
+        .await
+}
