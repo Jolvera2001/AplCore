5956714d92889d273c4632987880665c76863273
Jolvera2001 <jolvera6@stedwards.edu>
2024-03-29 19:32:19 -0500

    setting up Home page test and set up cors

--- aplfrontend/src/routes/Home.jsx
+++ aplfrontend/src/routes/Home.jsx
@@ -1,4 +1,22 @@
+import React, { useState, useEffect } from 'react'
+import {
+    Container,
+    Box,
+    Card,
+    CardHeader,
+    CardFooter,
+    CardBody
+} from '@chakra-ui/react'
+
 function Home() {
+    const [data, setData] = useState([]); // empty list
+
+    useEffect(() => {
+        fetch('http://localhost:8080/application/6600ed08f2a198fc24d51273')
+            .then(response => response.json())
+            .then(data => setData(data));
+    }, []);
+
     return (
         <div>
             <h1>Home</h1>
