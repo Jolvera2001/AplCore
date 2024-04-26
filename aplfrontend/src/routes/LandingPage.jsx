bafba7c9c2f2f9a24f291ee5982eeb00801b2162
Jolvera2001 <jolvera6@stedwards.edu>
2024-04-26 00:06:30 -0500

    experimenting with paralax

--- aplfrontend/src/routes/LandingPage.jsx
+++ aplfrontend/src/routes/LandingPage.jsx
@@ -18,56 +18,59 @@ import {
     Card,
     CardHeader,
     CardBody,
-    CardFooter
+    CardFooter,
 } from '@chakra-ui/react';
 
 function LandingPage() {
     return(
         <>
             <VStack>
-                <Box w="100%" h='1/3' py={16}>
-                    <HStack>
+                <Box w="100%" h='80vh' py={32} bgGradient='linear(to-r, red.500, red.400)'>
+                    <HStack mt={8}>
                         <Container maxW='4xl' p={5} textAlign='left'>
                             <Heading size='xl' my={2}>Application Tracking</Heading>
-                            <Heading size='lg' as='i' pl={5} my={2}>Simplified</Heading>
-                            <Text fontSize='xl' mt={5} w='33%' my={5}>AplCore aims to make your application process easier and simple</Text>
-                            <Button size='lg' m={2}>Register now!</Button>
+                            <Heading size='lg' as='i' as='u' pl={5} my={2}>Simplified</Heading>
+                            <Text fontSize='xl' mt={5} w='45%' my={5} pl={2}>AplCore aims to make your application process easier and simple. Whether you're searching for you next job or browsing, we aim to make it a smooth process.</Text>
+                            <Button size='lg'>Register now!</Button>
                         </Container>
+                        
                     </HStack>
                 </Box>
-                <Box w="100%" h='1/3' py={16} px={2}>
-                    <Grid>
-                        <GridItem row>
-                            <Card>
-                                <CardHeader>
-                                    <Heading size='md'>Easy to use</Heading>
-                                </CardHeader>
-                                <CardBody>
-                                    <Text>Simple and easy to use interface! Even if you have hundreds of applications, we make it easu for you to track them</Text>
-                                </CardBody>
-                            </Card>
-                        </GridItem>
-                        <GridItem>
-                            <Card>
-                                <CardHeader>
-                                    <Heading size='md'>Simply Add</Heading>
-                                </CardHeader>
-                                <CardBody>
-                                    <Text>Add a new application to track with a click of a button.</Text>
-                                </CardBody>
-                            </Card>
-                        </GridItem>
-                        <GridItem>
-                            <Card>
-                                <CardHeader>
-                                    <Heading size='md'>Filter easily</Heading>
-                                </CardHeader>
-                                <CardBody>
-                                    <Text>Look through all your application through different filters or search for what you named it</Text>
-                                </CardBody>
-                            </Card>
-                        </GridItem>
-                    </Grid>
+                <Box w="100%" py={16} px={2} my={8}>
+                    <Center>
+                        <Grid templateRows='repeat(1, 1fr)' templateColumns='repeat(3, 1fr)' gap={4}>
+                            <GridItem rowSpan={1} colSpan={1}>
+                                <Card maxW='25vw' h='25vh'>
+                                    <CardHeader>
+                                        <Heading size='md'>Easy to use</Heading>
+                                    </CardHeader>
+                                    <CardBody>
+                                        <Text>Simple and easy to use interface! Even if you have hundreds of applications, we make it easu for you to track them</Text>
+                                    </CardBody>
+                                </Card>
+                            </GridItem>
+                            <GridItem rowSpan={1} colSpan={1}>
+                                <Card maxW='25vw' h='25vh'>
+                                    <CardHeader>
+                                        <Heading size='md'>Simply Add</Heading>
+                                    </CardHeader>
+                                    <CardBody>
+                                        <Text>Add a new application to track with a click of a button! Edit mistakes or add new information with another button!</Text>
+                                    </CardBody>
+                                </Card>
+                            </GridItem>
+                            <GridItem rowSpan={1} colSpan={1}>
+                                <Card maxW='25vw' h='25vh'>
+                                    <CardHeader>
+                                        <Heading size='md'>Filter easily</Heading>
+                                    </CardHeader>
+                                    <CardBody>
+                                        <Text>Look through all your application through different filters or search for what you named it</Text>
+                                    </CardBody>
+                                </Card>
+                            </GridItem>
+                        </Grid>
+                    </Center>
                 </Box >
             </VStack>
         </>
